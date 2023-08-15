e767d9b78ccbe667fc1b4067d3338172e41225fc
Threatgrid <threatgrid@cisco.com>
2023-08-15 15:05:54 +0000

    Version 0.26.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.threatgrid/ring-swagger "0.26.3-SNAPSHOT"
+(defproject io.github.threatgrid/ring-swagger "0.26.3"
   :description "Swagger Spec for Ring Apps"
   :url "https://github.com/metosin/ring-swagger"
   :license {:name "Eclipse Public License"
